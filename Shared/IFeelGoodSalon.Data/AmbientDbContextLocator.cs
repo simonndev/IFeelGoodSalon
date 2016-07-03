cc25fac6a523c3714d40f332275a13ae221ec09c
Simon Nguyen <simonn.dev@gmail.com>
2016-07-04 01:07:28 +0700

    Added AmbientDbContextLocator concrete class

---
+++ Shared/IFeelGoodSalon.Data/AmbientDbContextLocator.cs
@@ -0,0 +1,15 @@
+﻿using IFeelGoodSalon.Data.Base;
+using System.Data.Entity;
+
+namespace IFeelGoodSalon.Data
+{
+    public class AmbientDbContextLocator : IAmbientDbContextLocator
+    {
+        public TDbContext Get<TDbContext>() where TDbContext : DbContext
+        {
+            var ambientDbContextScope = DbContextScope.GetAmbientScope();
+
+            return ambientDbContextScope == null ? null : ambientDbContextScope.Manager.Get<TDbContext>();
+        }
+    }
+}
