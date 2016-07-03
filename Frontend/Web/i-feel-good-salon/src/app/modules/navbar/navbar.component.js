799f91869d77ec52d366875a12854a5de8ccd0ec
Simon Nguyen <simonn.dev@gmail.com>
2016-07-04 01:09:47 +0700

    Added gulp-angular angular-material website source.

---
+++ Frontend/Web/i-feel-good-salon/src/app/modules/navbar/navbar.component.js
@@ -0,0 +1,16 @@
+﻿import template from './navbar.html';
+import controller from './navbar.controller';
+import './_navbar.scss';
+
+let navbarComponent = () => {
+  return {
+    restrict: 'E',
+    scope: {},
+    template,
+    controller,
+    controllerAs: 'vm',
+    bindToController: true
+  };
+};
+
+export default navbarComponent;
---
+++ Frontend/Web/i-feel-good-salon/src/app/modules/navbar/navbar.js
@@ -0,0 +1,8 @@
+﻿import navbarComponent from './navbar.component';
+
+let navbarModule = angular.module('navbar', [])
+  .run(calendarRun)
+  .directive('navbar', calendarComponent)
+  .service('navbarService', calendarService);
+
+export default navbarModule;
